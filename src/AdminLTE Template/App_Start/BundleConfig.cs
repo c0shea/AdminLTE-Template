25caeeea48852a73842725198f7794411967eca9
Connor O'Shea <connor@connoroshea.com>
2016-07-31 10:57:35 -0400

    Added plugins

--- src/AdminLTE Template/App_Start/BundleConfig.cs
+++ src/AdminLTE Template/App_Start/BundleConfig.cs
@@ -5,14 +5,20 @@ namespace AdminLTE_Template.App_Start {
         public static void RegisterBundles(BundleCollection bundles) {
             bundles.Add(new StyleBundle("~/Bundles/css")
                 .Include("~/Content/css/bootstrap.css")
-                .Include("~/Content/css/font-awesome.css")
+                .Include("~/Content/css/select2.css")
+                .Include("~/Content/css/datepicker3.css")
                 .Include("~/Content/css/AdminLTE.css")
                 .Include("~/Content/css/skins/skin-blue.css"));
 
             bundles.Add(new ScriptBundle("~/Bundles/js")
-                .Include("~/Content/js/plugins/jquery-2.2.4.js")
-                .Include("~/Content/js/plugins/bootstrap.js")
-                .Include("~/Content/js/plugins/fastclick.js")
+                .Include("~/Content/js/plugins/jquery/jquery-2.2.4.js")
+                .Include("~/Content/js/plugins/bootstrap/bootstrap.js")
+                .Include("~/Content/js/plugins/fastclick/fastclick.js")
+                .Include("~/Content/js/plugins/slimscroll/jquery.slimscroll.js")
+                .Include("~/Content/js/plugins/select2/select2.full.js")
+                .Include("~/Content/js/moment/moment.js")
+                .Include("~/Content/js/plugins/datepicker/bootstrap-datepicker.js")
+                .Include("~/Content/js/plugins/icheck.js")
                 .Include("~/Content/js/app.js"));
 
 #if DEBUG
