7be74a81d6d79e6525c9d0d175963349e922710c
Connor O'Shea <connor@connoroshea.com>
2016-07-30 22:49:43 -0400

    Restructured dependencies

---
+++ src/AdminLTE Template/App_Start/BundleConfig.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Optimization;
+
+namespace AdminLTE_Template.App_Start {
+    public class BundleConfig {
+        public static void RegisterBundles(BundleCollection bundles) {
+            bundles.Add(new StyleBundle("~/Bundles/css")
+                .Include("~/Content/css/bootstrap.css")
+                .Include("~/Content/css/font-awesome.css")
+                .Include("~/Content/css/AdminLTE.css")
+                .Include("~/Content/css/skins/skin-blue.css"));
+
+            bundles.Add(new ScriptBundle("~/Bundles/js")
+                .Include("~/Content/js/plugins/jquery-2.2.4.js")
+                .Include("~/Content/js/plugins/bootstrap.js")
+                .Include("~/Content/js/plugins/fastclick.js")
+                .Include("~/Content/js/app.js"));
+
+#if DEBUG
+            BundleTable.EnableOptimizations = false;
+#else
+            BundleTable.EnableOptimizations = true;
+#endif
+        }
+    }
+}
--- src/AdminLTE Template/Global.asax.cs
+++ src/AdminLTE Template/Global.asax.cs
@@ -1,18 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
+using AdminLTE_Template.App_Start;
 
 namespace AdminLTE_Template
 {
-    public class MvcApplication : System.Web.HttpApplication
+    public class MvcApplication : HttpApplication
     {
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
