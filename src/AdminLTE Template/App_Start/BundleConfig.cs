e6ad4e8c5d605e6bd6254870ca6249ff4dcee212
Connor O'Shea <connor@connoroshea.com>
2017-01-15 14:12:26 -0500

    Updated Inputmask to 3.3.4

--- src/AdminLTE Template/App_Start/BundleConfig.cs
+++ src/AdminLTE Template/App_Start/BundleConfig.cs
@@ -20,6 +20,7 @@ namespace AdminLTE_Template.App_Start {
                 .Include("~/Content/js/plugins/datepicker/bootstrap-datepicker.js")
                 .Include("~/Content/js/plugins/icheck/icheck.js")
                 .Include("~/Content/js/plugins/validator.js")
+                .Include("~/Content/js/plugins/inputmask/jquery.inputmask.bundle.js")
                 .Include("~/Content/js/app.js")
                 .Include("~/Content/js/init.js"));
 
--- src/AdminLTE Template/Content/js/init.js
+++ src/AdminLTE Template/Content/js/init.js
@@ -9,4 +9,8 @@
         checkboxClass: "icheckbox_minimal-blue",
         radioClass: "iradio_minimal-blue"
     });
+
+    $("#datemask").inputmask("dd/mm/yyyy", { "placeholder": "dd/mm/yyyy" });
+    $("#datemask2").inputmask("mm/dd/yyyy", { "placeholder": "mm/dd/yyyy" });
+    $("[data-mask]").inputmask();
 });
