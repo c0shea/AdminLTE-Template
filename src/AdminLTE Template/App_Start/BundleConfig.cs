4409e3633e4ee143339cb438dee3414f8aa1c50c
Connor O'Shea <connor@connoroshea.com>
2017-07-20 22:18:52 -0400

    Add BundleConfig and RouteConfig

---
+++ src/AdminLTE Template/App_Start/BundleConfig.cs
@@ -0,0 +1,37 @@
+﻿using System.Web.Optimization;
+
+namespace $safeprojectname$.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/Bundles/css")
+                .Include("~/Content/css/bootstrap.css")
+                .Include("~/Content/css/select2.css")
+                .Include("~/Content/css/datepicker3.css")
+                .Include("~/Content/css/AdminLTE.css")
+                .Include("~/Content/css/skins/skin-blue.css"));
+
+            bundles.Add(new ScriptBundle("~/Bundles/js")
+                .Include("~/Content/js/plugins/jquery/jquery-2.2.4.js")
+                .Include("~/Content/js/plugins/bootstrap/bootstrap.js")
+                .Include("~/Content/js/plugins/fastclick/fastclick.js")
+                .Include("~/Content/js/plugins/slimscroll/jquery.slimscroll.js")
+                .Include("~/Content/js/plugins/select2/select2.full.js")
+                .Include("~/Content/js/plugins/moment/moment.js")
+                .Include("~/Content/js/plugins/datepicker/bootstrap-datepicker.js")
+                .Include("~/Content/js/plugins/icheck/icheck.js")
+                .Include("~/Content/js/plugins/validator.js")
+                .Include("~/Content/js/plugins/inputmask/jquery.inputmask.bundle.js")
+                .Include("~/Content/js/app.js")
+                .Include("~/Content/js/init.js"));
+
+#if DEBUG
+            BundleTable.EnableOptimizations = false;
+#else
+            BundleTable.EnableOptimizations = true;
+#endif
+        }
+    }
+}
