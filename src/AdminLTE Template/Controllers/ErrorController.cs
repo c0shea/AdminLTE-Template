7f012753343f50ae91f764a0d92f1cfaefb63d4f
Connor O'Shea <connor@connoroshea.com>
2017-12-14 22:07:37 -0500

    Add ErrorController to handle 404s and 500s

---
+++ src/AdminLTE Template/Controllers/ErrorController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+
+namespace $safeprojectname$.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public ActionResult InternalServerError()
+        {
+            return View();
+        }
+
+        public ActionResult NotFound()
+        {
+            return View();
+        }
+    }
+}
--- src/AdminLTE Template/Global.asax.cs
+++ src/AdminLTE Template/Global.asax.cs
@@ -1,8 +1,11 @@
-﻿using System.Web;
+﻿using System;
+using System.Net;
+using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 using $safeprojectname$.App_Start;
+using $safeprojectname$.Controllers;
 
 namespace $safeprojectname$
 {
@@ -14,5 +17,40 @@ namespace $safeprojectname$
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
-    }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            var exception = Server.GetLastError();
+            var httpContext = ((HttpApplication)sender).Context;
+            httpContext.Response.Clear();
+            httpContext.ClearError();
+
+            if (new HttpRequestWrapper(httpContext.Request).IsAjaxRequest())
+            {
+                return;
+            }
+
+            ExecuteErrorController(httpContext, exception as HttpException);
+        }
+
+        private void ExecuteErrorController(HttpContext httpContext, HttpException exception)
+        {
+            var routeData = new RouteData();
+            routeData.Values["controller"] = "Error";
+
+            if (exception != null && exception.GetHttpCode() == (int)HttpStatusCode.NotFound)
+            {
+                routeData.Values["action"] = "NotFound";
+            }
+            else
+            {
+                routeData.Values["action"] = "InternalServerError";
+            }
+
+            using (Controller controller = new ErrorController())
+            {
+                ((IController)controller).Execute(new RequestContext(new HttpContextWrapper(httpContext), routeData));
+            }
+        }
+}
 }
