089ba728de6d38d6989340803d600db3b683ec15
Connor O'Shea <connor@connoroshea.com>
2018-12-04 20:51:59 -0500

    Fix white space on resize (fix #19)

--- src/AdminLTE Template/Content/js/init.js
+++ src/AdminLTE Template/Content/js/init.js
@@ -11,4 +11,12 @@
     $("#datemask").inputmask("dd/mm/yyyy", { "placeholder": "dd/mm/yyyy" });
     $("#datemask2").inputmask("mm/dd/yyyy", { "placeholder": "mm/dd/yyyy" });
     $("[data-mask]").inputmask();
+
+    // Fix sidebar white space at bottom of page on resize
+    $(window).on("load", function () {
+        setTimeout(function () {
+            $("body").layout("fix");
+            $("body").layout("fixSidebar");
+        }, 250);
+    });
 });
