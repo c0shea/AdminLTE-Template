51181767ed567626d3efc7910d8a8e3f2d5ac7ed
Connor O'Shea <connor@connoroshea.com>
2019-03-10 21:26:58 -0400

    Restrict default controller actions to HttpGet (close #16)

--- src/AdminLTE Template/Controllers/ErrorController.cs
+++ src/AdminLTE Template/Controllers/ErrorController.cs
@@ -4,11 +4,13 @@ namespace $safeprojectname$.Controllers
 {
     public class ErrorController : Controller
     {
+        [HttpGet]
         public ActionResult InternalServerError()
         {
             return View();
         }
 
+        [HttpGet]
         public ActionResult NotFound()
         {
             return View();
--- src/AdminLTE Template/Controllers/HomeController.cs
+++ src/AdminLTE Template/Controllers/HomeController.cs
@@ -4,11 +4,13 @@ namespace $safeprojectname$.Controllers
 {
     public class HomeController : Controller
     {
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
         }
 
+        [HttpGet]
         public ActionResult AnotherLink()
         {
             return View("Index");
