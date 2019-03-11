8e362ed1e43e1c13f15b09d10cb5f07de83ae759
Connor O'Shea <connor@connoroshea.com>
2019-03-10 21:25:32 -0400

    Add support for VS 2019 (close #21)

--- src/AdminLTE Template/Properties/AssemblyInfo.cs
+++ src/AdminLTE Template/Properties/AssemblyInfo.cs
@@ -24,13 +24,12 @@ using System.Runtime.InteropServices;
 
 // Version information for an assembly consists of the following four values:
 //
-//      Major Version
-//      Minor Version 
-//      Build Number
+//      Major Version: When you make incompatible API changes
+//      Minor Version: When you add functionality in a backwards-compatible manner
+//      Build Number:  When you make backwards-compatible bug fixes
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
